37a09fc3d8e8991201b80862b6c07b7c359598f8
NisioN <nisiohelandeoliveira@gmail.com>
2026-01-22 21:37:21 -0400

    corrigindo a localização do banco

--- data/database.py
+++ data/database.py
@@ -1,6 +1,9 @@
 import sqlite3
+import os
 
-DB_NAME = "minhas_receitas.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_NAME = os.path.join(BASE_DIR, "..", "minhas_receitas.db")
+
 
 class Database:
     def __init__(self):
